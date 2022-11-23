35207b22819dd7595f92950fef39b9eb471ebf81
Khlopotnoy-Romadov <99768668+Khlopotnoy-Romadov@users.noreply.github.com>
2022-11-23 22:38:09 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 from PyQt5.QtWidgets import QAbstractItemView
-from PyQt5.uic.properties import QtGui
 import interface
 from PyQt5 import QtWidgets
-from pyowm.utils.config import get_default_config
 from best_product import list_of_purchases
 
 class Main(QtWidgets.QMainWindow, interface.Ui_MainWindow):
@@ -13,7 +11,6 @@ class Main(QtWidgets.QMainWindow, interface.Ui_MainWindow):
         self.pushButton_searchAddress.pressed.connect(self.go)
         self.listWidget_goods.setSelectionMode(QAbstractItemView.MultiSelection)
         self.pushButton_searchGoods.pressed.connect(self.result)
-        # self.pushButton_close.pressed.connect(self.close)
 
     def go(self):
         street = self.lineEdit_street.text()
