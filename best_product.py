4f7ec9cb84bf0d63776a305ec4037f034176b771
DanyaNewSkater <100216013+DanyaNewSkater@users.noreply.github.com>
2022-11-23 13:23:36 +0300

    Update best_product.py

--- best_product.py
+++ best_product.py
@@ -1,8 +1,10 @@
-from main import shops
+from data import shops
+from adresses import df
+
 print(shops)
 print()
 
-def list_of_purchases(lst: list)->list:
+def list_of_purchases(lst: list)->dict:
     values = dict()
     for i in lst:
         for j in shops.keys():
@@ -10,16 +12,33 @@ def list_of_purchases(lst: list)->list:
                 values[j] += shops[j][i]
             else:
                 values[j] = shops[j][i]
-    print(values)
+    return values
+
+print(list_of_purchases(["колбаса", "огурцы"]))
+
+def distance(adress: str) -> dict:
+    dist = dict()
+    for i in shops.keys():
+        dist[i] = df.loc[adress, shops[i]['адрес']]
+    return dist
+
+print(distance("ул. Авачинская, д. 20"))
+
+def best_shop(lst: list, adress: str, v_imp = 2, d_imp = 1) -> list:
+    val = list_of_purchases(lst)
+    dis = distance(adress)
+    res = dict()
+    for i in val.keys():
+        res[i] = val[i]*v_imp + dis[i]*d_imp//2
     min = 10000000
     min_mag = ""
-    for i in values.keys():
-        if values[i]<min:
-            min = values[i]
+    for i in res.keys():
+        if res[i] < min:
+            min = res[i]
             min_mag = i
     return [min_mag, shops[min_mag]['адрес']]
 
-print(list_of_purchases(["колбаса", "огурцы"]))
+print(best_shop(["колбаса", "огурцы"], "ул. Авачинская, д. 20"))
 
 
 
